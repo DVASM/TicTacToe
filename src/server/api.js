7536952a6b56629c7fa02432e5a5b64c19e96e9f
Viktor Karason <vingik@gmail.com>
2018-10-18 23:35:14 +0000

    setup of webpage on localhost and developer manual2

--- src/server/api.js
+++ src/server/api.js
@@ -1,1 +1,13 @@
-// api.js
+// src/server/api.js
+const express = require("express");
+const router = express.Router();
+const tictactoe = require("./../logic/tictactoe");
+router.get("/tictactoe", (req, res) => {
+    // Fill in your code that:
+   
+    // 1. set's the status code to 200
+   res.status(200).send("hello World of");
+    // 2. and returns an object with the greeting
+    // Hint: `req.params.name`
+   });
+   module.exports = router;
--- src/server/api.test.js
+++ src/server/api.test.js
@@ -1,1 +1,13 @@
-// api.test.js
+// src/server/api.test.js
+const request = require("supertest");
+const api = require("./api");
+describe("GET /tictactoe/ endpoint", () => {
+    it("should return a 200 OK status code", async () => {
+    const res = await request(api).get("/tictactoe");
+    expect(res.status).toBe(200);
+    });
+    it("should return the greeting in a object", async () => {
+    const res = await request(api).get("/tictactoe");
+    expect(res.body.greeting).toBe("Hello World");
+    });
+});
